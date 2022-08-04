2a404ec7e08d61a3c0cbb8dde4b39d18f59eaf58
Ata <nigilistkaa@gmail.com>
2022-08-05 00:50:12 +0300

    add random

--- task003_Sum/Program.cs
+++ task003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿// сложение двух заданных чисел и вывод суммы
-int numberA = 3;
-int numberB = 5;
+int numberA = new Random().Next(1, 10); //1234...9
+Console.WriteLine(numberA);
+int numberB = new Random ().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA+numberB;
 Console.WriteLine(result);
 

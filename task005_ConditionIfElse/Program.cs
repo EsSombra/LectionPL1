62350cb15fc5cdaedeb48c980a089aa34f09052e
Ata <nigilistkaa@gmail.com>
2022-08-05 01:10:18 +0300

    add task005 If Else

---
+++ task005_ConditionIfElse/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mary")
+{
+    Console.WriteLine("Wow, that's MARY!");
+}
+
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
+
+
